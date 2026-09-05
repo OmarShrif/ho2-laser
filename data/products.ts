8ba61ea03acae20de3be4351b7b2799cc06a0ffa
OmarShrif <omarshrif335@gmail.com>
2026-09-05 13:26:34 +0200

    Edited All

--- data/products.ts
+++ data/products.ts
@@ -860,7 +860,7 @@ export const products: Product[] = [
         slug: "nature",
         title: "Nature",
         category: "Custom Gifts",
-        image: "/products/nature.jpeg",
+        image: "/products/nature.jpg",
         material: "7mm MDF",
         size: "45 × 45 cm",
         description:
